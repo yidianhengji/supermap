08544e3921c909ac2521ca28463a7c4d90aeade4
yidianhengji <493871850@qq.com>
2019-06-03 15:28:52 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <sm-viewer @ready=ready navigation fullscreenButton>
+   </sm-viewer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.content {
+  background-color: #f9f9f9;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+</style>
